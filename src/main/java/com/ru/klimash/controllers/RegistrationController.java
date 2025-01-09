83c584db5fde43fc5c8d33c015c6e18dc918dbd3
detefuze <klimashd@gmail.com>
2025-01-09 21:16:37 +0300

    09.01.2025

--- src/main/java/com/ru/klimash/controllers/RegistrationController.java
+++ src/main/java/com/ru/klimash/controllers/RegistrationController.java
@@ -26,22 +26,26 @@ public class RegistrationController {
     }
 
     @GetMapping("/registration")
-    public String registrationForm(Model model) {
-        List<Customer> customersList = customersService.getFioAndEmail();
-        model.addAttribute("customers", customersList);
+    public String registrationForm() {
         return "registration-form";
+    }
+
+    @GetMapping("/registration_success")
+    public String registrationComplete() {
+        return "registration-success";
     }
 
     @PostMapping("/registration/new_customer")
     public String registration(@RequestParam String fio,
-                                               @RequestParam String email,
-                                               @RequestParam String password) {
+                               @RequestParam String email,
+                               @RequestParam String password) {
 
         Customer customer = new Customer();
         customer.setFio(fio);
         customer.setEmail(email);
         customer.setPassword(password);
         customersRepository.save(customer);
-        return "redirect:/main_menu/registration_api/registration";
+        return "redirect:http://localhost:8081/main_menu/registration_api" +
+                "/registration_success";
     }
 }
